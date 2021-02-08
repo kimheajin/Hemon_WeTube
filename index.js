0334f8473b33ab64fe7a50c6d3fb033e93d3aa44
KimHeajin <goodcandy200@gmail.com>
2021-02-08 23:44:55 +0900

    start expressjs and my first server

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Please git commit!!");
+var express = require('express');
+var app = express();
+
+const POST = 4000;
+
+function handleListen(){
+    console.log("Listening on : http://localhost:4000");
+}
+
+function handleHome(req, res){
+    console.log(req);
+    res.send("Hello from home");
+}
+
+function handleProfile(req, res){
+    res.send("yoou are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile",handleProfile);
+
+app.listen(POST, handleListen);
