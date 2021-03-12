d96fa7a6be85652aa852157efa46d135f019c2fb
KimHeajin <goodcandy200@gmail.com>
2021-03-12 15:18:42 +0900

    Dot Env Creating and add gitignore

--- db.js
+++ db.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+// env안에 있는 정보를 불러올 수 있다.
+dotenv.config();
 
+// 아래에서 요청하는 건 String으로 된 Database이며 어디에 Database가 저장되어 있는 지 알려준다.
 mongoose.connect(
-    "mongodb://localhost:27017/we-tube", 
+    // port를 확인하는 방법은 cmd창에 mongod를 입력하여 나오는 정보를 가지고 확인.
+    // mongoDB의 URL을 dotenv로 숨겼다.
+    process.env.MONGO_URL,
     {
         useNewUrlParser:true,
         useFindAndModify:false,
@@ -11,6 +17,7 @@ mongoose.connect(
 
 const db = mongoose.connection;
 
+// 성공 여부를 알 수 있는 함수
 const handleOpen = () => console.log("Connected to DB");
 const handleError = (error) => console.log(`Error on DB Connection:${error}`);
 
--- init.js
+++ init.js
@@ -1,7 +1,10 @@
 import "./db.js"
 import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 4000;
+// PORT번호를 dotenv로 숨겼다.
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
